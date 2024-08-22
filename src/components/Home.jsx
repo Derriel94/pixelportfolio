e270262d2fa17a7f9d07898a7dc83d25b48f506a
Derriel Collins <derrielcollins96@gmail.com>
2024-08-22 01:39:42 -0500

    changed to regular slider

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,16 @@
 import React,{ useState } from 'react'
-import { motion } from "framer-motion";
+import { motion, useTransform, useMotionValue, useMotionValueEvent } from "framer-motion";
+
 
 
 const Home = () => {
 	const randompic = 'https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/18ad0649-f2dd-410d-8842-2ba2aa486fdb/915b499c07b086b46c4d52e937a9a44c29568f13.jpg?submitted=1684654124201&host=wbd-images.prod-vod.h264.io&partner=beamcom&w=320'
-	const slider = {
-		hidden: {
-
-		},
-		visible: {
-
-		}
-	}
+	
 	let array = [];
 
-	let string = "PIXEL ART AND GAMES!"
+	let string = "PIXEL-ART-AND-GAMES!"
 	array = [...string]
 	
-
 
 
 	const colorList = ["red", "purple", "yellow", "green", "orange"];
@@ -40,26 +33,11 @@ const Home = () => {
 	
 	let displayArray = [{},{},{}];
 
-			//I have to loop through currentItems to access this or go curentItems[0,1,2].title or imgSrc												
 	const [currentItems, setCurrentItems] = useState(displayArray)
-	
-	// const handle3partobjectmovement = (array) => {
-	
-	// 	let nextIndex = currentIndex + 1;
-	// 	let prevIndex = currentIndex != 0 ? currentIndex - 1 : array.length - 1;
-	
-	// }
-
-	// click next
-	// loop through projectList
-	// grab the next three currentItems
-	// display them
 
 	const handleNext = (e) => {
 		let currentIndex = 0;
-		// 	setCurrentItem(projectList[currentIndex + 1])
-		 // setNextItem(currentIndex + 2)
-		 // setPrevItem(projectList[currentIndex -1])
+
 		
 		if (currentIndex == 0) {
 			console.log('here')
@@ -72,19 +50,37 @@ const Home = () => {
 		
 	}
 
-	// const handlePrev = (e) => {
-	// 	if (currentIndex == 0) {
-	// 		setPrevItem(projectList[projectList.length - 1])
-	// 		setCurrentItem(projectList[currentIndex + 1]);
-	// 		setNextItem(projectList[currentIndex + 2]);
-	// 	}
-	// }
+	const x = useMotionValue(0);
+
+	useMotionValueEvent(x, "change", (yo) => console.log(yo))
+	const move = useTransform(x, [-window.innerWidth, 0,360], ["-360","0","20" ])
 
 
 
 	return (
 	<div className="home">
-
+				<motion.div 
+					drag="x"
+				   	dragConstraints={{
+				      left: -window.innerWidth,
+				      right: 200,
+				    }}
+				    style={{x,
+							move}}
+        		className="carousel">
+        				<h1>ACCESS THE VAULT</h1>
+        			{
+        				projectList.map(project=>{
+        			return(
+        			
+        			<motion.div 
+        			className="carousel-main-img">
+	          			<h3>{project.title}</h3>
+	          			<img src={project.imgSrc}/>
+	          		</motion.div>);
+        				})
+        			}
+	          	</motion.div>
         	<div className="top-container">
         		<div className="hometitle">
         		{array.map(letter=>{  
@@ -99,37 +95,14 @@ const Home = () => {
         		<div className="personal-image">
           			<img src="https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/18ad0649-f2dd-410d-8842-2ba2aa486fdb/915b499c07b086b46c4d52e937a9a44c29568f13.jpg?submitted=1684654124201&host=wbd-images.prod-vod.h264.io&partner=beamcom&w=320"/>
           		</div>
-          		<p>My goal is to provide a oppurtunity for everyone
+          		<p>My goal is to provide an oppurtunity for everyone
           		to have the game of there dreams, or at least be able to 
           		have there favorite tv show or movie turned into a game!</p>
         	</div>
       
-        		<div className="carousel-container">
-        		<motion.div className="carousel">
-        			{
-        				projectList.map(project=>{
-        			return(
-        			
-        			<div className="carousel-main-img">
-	          			<h3>{project.title}</h3>
-	          			<img src={project.imgSrc}/>
-	          		</div>);
-        				})
-        			}
-	        	{/*	<div onClick={(e)=>handleNext(e)} className="carousel-top-img">
-	        			<h3>{projectList[1].title}</h3>
-	          			<img src={projectList.imgSrc}/>
-	          		</div>
-	          		<div className="carousel-main-img">
-	          			<h3>{projectList[0].title}</h3>
-	          			<img src={projectList.imgSrc}/>
-	          		</div>
-	          		<div onClick={(e)=>handlePrev(e)} className="carousel-bottom-img">
-	          			<h3>{projectList[6].title}</h3>
-	          			<img src={projectList.imgSrc}/>
-	          		</div>*/}
-	          	</motion.div>
-	          	</div>
+
+
+
         
       
         

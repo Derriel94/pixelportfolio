800c902db95e6df4f1bb3c43eb495a086f6f5189
Derriel Collins <derrielcollins96@gmail.com>
2024-08-28 11:42:40 -0500

    added final section and an alert that says recomended for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React,{ useState } from 'react'
+import React,{ useState, useEffect } from 'react'
 import './App.css';
 import Router from './router.jsx'
-function App() {
 
+function App() {
   const title = ['D','E','R','R','I','E','L','-','C','O','L','L','I','N','S','-','P','O','R','T','O','L','I','O'];
   const colorList = ["red", "purple", "yellow", "green", "orange"];
 
@@ -18,7 +18,29 @@ function App() {
         return (<h1 id={colorList[inorderNumber++]}>{letter} </h1>)
     })
    }
-  
+
+    const [windowWidth, setWindowWidth] = useState(screen.width)
+    useEffect(() => {
+      setWindowWidth(screen.width)
+
+    console.log(windowWidth)
+    console.log(screen.width)
+    if (localStorage.getItem("seenpop")) {
+
+
+    } else {
+        alert("optimized for desktop or laptop. Mobile: not reccomended")
+        localStorage.setItem("seenpop", true)
+
+    }
+  }, [windowWidth])
+
+    setTimeout(()=> {
+        localStorage.removeItem("seenpop", true)
+
+    }, 150000)
+    // 1000 = 1secon
+
   return (
     <div>
        <div className="header">
@@ -37,9 +59,14 @@ function App() {
 
       </div>
      <Router />
-          <h1 style={{textAlign: "center", marginTop: "100px"}}>WELCOME TO MY WORLD!</h1>  
-            <img style={{marginLeft: "auto"}} src="./../public/flamewizardfinal.gif" />
+       <h1 style={{textAlign: "center", marginTop: "100px"}}>WELCOME TO MY WORLD!</h1>  
+        <div className="game-section">
+            <div><img  src="./../public/flamewizardfinal.gif" /></div>
+                <div className="game-window">
 
+                </div>
+            <div className={ windowWidth < 500? "hidden": "show"}><img  src="./../public/flamewizardfinal.gif" /></div>
+        </div>
     </div>
   )
 }
